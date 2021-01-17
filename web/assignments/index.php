beb4a9760942db0fe64d3c2f4eb05f1ed236216d
bhogge2 <bradfordhogge@gmail.com>
2021-01-16 21:46:58 -0800

    added notes and assignments

---
+++ web/assignments/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Backend 2 Homepage</title>
+    <link rel="stylesheet" href="styles/screen.css">
+</head>
+
+<body>
+    <div class="content">
+        <nav>
+            <div>
+                <a href="/home.php">Introduction</a>
+                <a href="/assignments.php">Assignments</a>
+            </div>
+        </nav>
+        
+        <?php echo "Today is " . date("D M d"); ?>
+    </div>
+</body>
+
+</html>
