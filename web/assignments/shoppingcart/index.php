12362b866a63ea64657fd88ba9bffe206cdfadda
bhogge2 <bradfordhogge@gmail.com>
2021-01-23 16:39:45 -0800

    test

--- web/assignments/shoppingcart/index.php
+++ web/assignments/shoppingcart/index.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
 $products_array = array (
-    array ('id' =>"1", 'name'=>"Chair", 'price'=>"20"),
-    array ('id' =>"2", 'name'=>"Matress", 'price'=>"70"),
-    array ('id' =>"3", 'name'=>"Desk", 'price'=>"50")
+    array ('id' =>"1", 'name'=>"Chair", 'price'=>"$20"),
+    array ('id' =>"2", 'name'=>"Matress", 'price'=>"$70"),
+    array ('id' =>"3", 'name'=>"Desk", 'price'=>"$50")
     );
 ?>
 
@@ -21,7 +21,13 @@ $products_array = array (
 <div class="productList">
     <?php
 
-foreach ($products_array as $product) {
+foreach ($products_array as $key=>$product) {
+    echo "<div class='product'><div class='productDetail'<p>$product[$key]['name']</p><p>$product[$key]['price']</p></div></div>";
+    }
+
+    ?>
+<!--
+foreach ($products_array as $key=>$product) {
     echo "<div class='product'>";
         foreach($product as $productdetail) {
         echo "<div class='productDetail'<p>$productdetail</p></div>";
@@ -30,6 +36,7 @@ foreach ($products_array as $product) {
     }
 
     ?>
+-->
     </div>
 </body>
 </html>
