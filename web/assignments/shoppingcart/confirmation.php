58306b36297c88c8c9b2e8e45a3a8d52a0e06d3e
bhogge2 <bradfordhogge@gmail.com>
2021-01-23 19:30:15 -0800

    test

--- web/assignments/shoppingcart/confirmation.php
+++ web/assignments/shoppingcart/confirmation.php
@@ -12,8 +12,27 @@ session_start();
     <title>Document</title>
 </head>
 <body>
+<?php include './common/header.php'; ?>
+<h1>Confirmation</h1>
     <?php
-        echo '<p>Address: ' . $_SESSION["address"]["street"] . ', ' . $_SESSION["address"]["city"] . ', ' . $_SESSION["address"]["state"] . ' ' . $_SESSION["address"]["zip"] . '</p>';
+        echo '<p>Items will be sent to: ' . $_SESSION["address"]["street"] . ', ' . $_SESSION["address"]["city"] . ', ' . $_SESSION["address"]["state"] . ' ' . $_SESSION["address"]["zip"] . '</p>';
+
+        if(!empty($_SESSION["shoppingCart"])) 
+        {
+            $total = 0;
+            foreach($_SESSION["shoppingCart"] as $keys => $values)
+            {
+                ?>
+                <div><?php echo $values["item_name"]; ?></div>
+                <div><?php echo $values["item_quantity"]; ?></div>
+                <div>$<?php echo number_format($values["item_quantity"] * $values["item_price"], 2); ?></div>
+                <?php
+                $total = $total + ($values["item_quantity"] * $values["item_price"]);
+            }   
+            ?>
+            <p>$<?php echo number_format($total, 2); ?></p>
+            <?php
+        }
     ?>
 </body>
 </html>
