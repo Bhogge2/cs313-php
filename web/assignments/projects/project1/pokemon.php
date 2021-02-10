3fb58487423e00dd82dbb3917d6cd840a285d9e4
bhogge2 <bradfordhogge@gmail.com>
2021-02-09 22:07:02 -0800

    db access

--- web/assignments/projects/project1/pokemon.php
+++ web/assignments/projects/project1/pokemon.php
@@ -17,21 +17,6 @@ require "../web/db/dbAccess.php";
     <label>Pokemon</label>
     <select name='pokemon_filter' id='pokemon_filter' required>
     <?php
-       $pokemon_array = array();
-       //Search for all the books in the DB 
-       foreach ($db->query ('SELECT DISTINCT pokemon_name FROM pokemon') as $row )
-       {
-           //Add book to the book array if it has not been added 
-          if(!(in_array($row['pokemon_name'], $pokemon_array)))
-          {
-              array_push($pokemon_array, $row['pokemon_name']);
-          }
-       }
-       //loop the books_array variable to create an option for each book 
-       foreach ($pokemon_array as $pokemon_in_array )
-       {
-        echo '<option value="' . $pokemon_in_array . '">' .  $pokemon_in_array . '</option>';
-       }
     ?>
     </select>
     <button type="submit">Submit Form</button>
