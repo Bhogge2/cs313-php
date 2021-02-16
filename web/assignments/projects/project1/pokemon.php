d52bafae3a8a9285996aecbc117b187dc35a7dfb
bhogge2 <bradfordhogge@gmail.com>
2021-02-15 18:28:56 -0800

    db

--- web/assignments/projects/project1/pokemon.php
+++ web/assignments/projects/project1/pokemon.php
@@ -1,6 +1,7 @@
 <?php
 require "../../../db/dbAccess.php";
 
+var_dump($db);
 ?>
 
 <!DOCTYPE html>
@@ -16,8 +17,28 @@ require "../../../db/dbAccess.php";
     <p>Search by:</p>
     <label>Pokemon</label>
     <select name='pokemon_filter' id='pokemon_filter' required>
+    
     <?php
-    ?>
+// Notice that we avoid using "SELECT *" here. This is considered
+// good practice so we don't inadvertently bring back data we don't
+// want, especially if the database changes later.
+$statement = $db->prepare("SELECT pokemon_name, pokemon_type, pokemon_type2 FROM pokemon");
+$statement->execute();
+
+// Go through each result
+while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+{
+	// The variable "row" now holds the complete record for that
+	// row, and we can access the different values based on their
+	// name
+	$pokemon_name = $row['pokemon_name'];
+	$pokemon_type = $row['pokemon_type'];
+	$pokemon_type2 = $row['pokemon_type2'];
+
+	echo "<p><strong>$pokemon_name $pokemon_type:$pokemon_type2</strong><p>";
+}
+
+?>
     </select>
     <button type="submit">Submit Form</button>
     <form> 
