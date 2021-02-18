a330178b4aaeb6e426982e7be78bf0a746cf4fe9
bhogge2 <bradfordhogge@gmail.com>
2021-02-17 22:58:43 -0800

    added more files

---
+++ web/assignments/projects/project1/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Pokemon Home</title>
+</head>
+
+<body>
+<ul class="nav justify-content-end">
+  <li class="nav-item">
+    <a class="nav-link active" href="pokemon.php">Pokemon</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Account</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="signIn.php">Sign In / Sign Up</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+  </li>
+</ul>
+    <div class="container">
+        <div class="row">
+
+        </div>
+    </div>
+</body>
+
+</html>
--- web/assignments/projects/project1/pokemon.php
+++ web/assignments/projects/project1/pokemon.php
@@ -1,46 +1,50 @@
 <?php
-require "../../../db/dbAccess.php";
+require "dbAccess.php";
 $db = get_db();
-var_dump($db);
 ?>
 
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Pokemon</title>
-    </head>
-    <body>
+
+<head>
+    <title>Pokemon</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
         <h1>Pokemon App</h1>
-        
-    <form method="post" action="/">
-    <p>Search by:</p>
-    <label>Pokemon</label>
-    <select name='pokemon_filter' id='pokemon_filter' required>
-    <option value="$pokemon_name"></option>
-    <?php
-    // Notice that we avoid using "SELECT *" here. This is considered
-// good practice so we don't inadvertently bring back data we don't
-// want, especially if the database changes later.
-$statement = $db->prepare("SELECT * FROM pokemon");
-$statement->execute();
 
-// Go through each result
-while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-{
-	// The variable "row" now holds the complete record for that
-	// row, and we can access the different values based on their
-	// name
-	$pokemon_name = $row['pokemon_name'];
-	$pokemon_type = $row['pokemon_type'];
-	$pokemon_type2 = $row['pokemon_type2'];
+        <form method="post" action="/">
+            <p>Search by:</p>
+            <label>Pokemon</label>
+            <select name='pokemon_filter' id='pokemon_filter' required>
+                <option value="$pokemon_name"></option>
+                <?php
+                // Notice that we avoid using "SELECT *" here. This is considered
+                // good practice so we don't inadvertently bring back data we don't
+                // want, especially if the database changes later.
+                $statement = $db->prepare("SELECT * FROM pokemon");
+                $statement->execute();
 
-	echo "<option value=" . $pokemon_name . ">" . $pokemon_name ."</option>";
-}
-    
-?>
-</select>
-    <button type="submit">Submit Form</button>
-    </form> 
+                // Go through each result
+                while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+                    // The variable "row" now holds the complete record for that
+                    // row, and we can access the different values based on their
+                    // name
+                    $pokemon_name = $row['pokemon_name'];
+                    $pokemon_type = $row['pokemon_type'];
+                    $pokemon_type2 = $row['pokemon_type2'];
+
+                    echo "<option value=" . $pokemon_name . ">" . $pokemon_name . "</option>";
+                }
+
+                ?>
+            </select>
+            <button type="submit">Submit Form</button>
+        </form>
+    </div>
+
 </html>
 
-    </body>
+</body>
