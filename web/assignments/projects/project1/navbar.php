4ddb139614ed510128e67e75cc7e671977fea61e
bhogge2 <bradfordhogge@gmail.com>
2021-02-18 20:30:14 -0800

    bootstrap

--- web/assignments/projects/project1/navbar.php
+++ web/assignments/projects/project1/navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-light light-blue">
     <div class="container">
         <a class="navbar-brand" href="index.php">Pokemon Favorites</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -7,7 +7,7 @@
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
+                <li class="nav-item">
                     <a class="nav-link" href="index.php">Home</span></a>
                 </li>
                 <li class="nav-item">
