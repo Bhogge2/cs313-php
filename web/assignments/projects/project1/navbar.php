2796ce1b7f828ecd30c0d4edbd4099e3bccbdf29
bhogge2 <bradfordhogge@gmail.com>
2021-02-18 20:20:48 -0800

    bootstrap

--- web/assignments/projects/project1/navbar.php
+++ web/assignments/projects/project1/navbar.php
@@ -1,20 +1,22 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="index.php">Pokemon Favorites</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
+<div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="index.php">Pokemon Favorites</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="index.php">Home</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="pokemon.php">Pokemon</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="signIn.php">Sign In / Sign Up</a>
-            </li>
-        </ul>
-    </div>
-</nav>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Home</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="pokemon.php">Pokemon</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="signIn.php">Sign In / Sign Up</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</div>
