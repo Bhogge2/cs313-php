d61320712b8dd7851af4d6c8f162892001bcdbbf
bhogge2 <bradfordhogge@gmail.com>
2021-02-20 15:32:23 -0800

    api

--- web/assignments/projects/project1/pokemon.php
+++ web/assignments/projects/project1/pokemon.php
@@ -7,14 +7,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pokemon</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+
     <link rel="stylesheet" href="pokemon.css">
 </head>
 
 <body>
 
     <body>
-        <?php include 'navbar.php'; ?>
+        <!--<php include 'navbar.php'; ?>-->
         <div class="container">
             <div class="content">
                 <input type="text" id="myInput" onkeyup="filterPokemon()" placeholder="Search for Pokemon..." title="Type in a Pokemon">
