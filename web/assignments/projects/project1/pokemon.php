3307583d2d8dec9b6773cdffa6d3f66de667e98e
bhogge2 <bradfordhogge@gmail.com>
2021-02-20 15:34:53 -0800

    api

--- web/assignments/projects/project1/pokemon.php
+++ web/assignments/projects/project1/pokemon.php
@@ -15,13 +15,11 @@
 
     <body>
         <!--<php include 'navbar.php'; ?>-->
-        <div class="container">
-            <div class="content">
-                <input type="text" id="myInput" onkeyup="filterPokemon()" placeholder="Search for Pokemon..." title="Type in a Pokemon">
-                <div class="listBox">
-                    <ul id="allList"></ul>
-                    <ul id="favoritesList" class="hide"></ul>
-                </div>
+        <div class="content">
+            <input type="text" id="myInput" onkeyup="filterPokemon()" placeholder="Search for Pokemon..." title="Type in a Pokemon">
+            <div class="listBox">
+                <ul id="allList"></ul>
+                <ul id="favoritesList" class="hide"></ul>
             </div>
         </div>
         <script src="GetPokemonAPI.js"></script>
