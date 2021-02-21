6ac84dbbb75987b34e7dfdc3dfe2301a70b01a6a
bhogge2 <bradfordhogge@gmail.com>
2021-02-20 21:57:46 -0800

    sign in up

--- web/assignments/projects/project1/index.php
+++ web/assignments/projects/project1/index.php
@@ -1,5 +1,7 @@
 <?php
 session_start();
+
+header("Location: pokemon.php");
 ?>
 
 <!DOCTYPE html>
@@ -11,11 +13,12 @@ session_start();
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" href="style.css">
+
   <title>Pokemon Home</title>
 </head>
 
 <body>
-  
+
   <?php include 'navbar.php'; ?>
   <div class="container big-container">
   </div>
