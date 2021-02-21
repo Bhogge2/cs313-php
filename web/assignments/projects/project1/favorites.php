40dd686210d23d7e4a3052bb64a8bd85d7f94b0a
bhogge2 <bradfordhogge@gmail.com>
2021-02-20 22:25:20 -0800

    sign in up

--- web/assignments/projects/project1/favorites.php
+++ web/assignments/projects/project1/favorites.php
@@ -7,6 +7,16 @@ $db = get_db();
 session_start();
 
 if (isset($_SESSION['user_id'])) {
+    $statement = $db->prepare("SELECT pokemon_id FROM user_favorites WHERE user_id=:userId");
+    $statement->bindValue(':userId', $_SESSION['user_id']);
+    $statement->execute();
+
+    $favoritesArray = array();
+
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+
+        array_push($favoritesArray, $row['pokemon_id']);
+    }
 } else {
     header("Location: signIn.php");
 }
@@ -28,25 +38,79 @@ if (isset($_SESSION['user_id'])) {
         <h1 style="text-align:center; color: white; text-shadow: 2px 2px 5px black; margin-top: 70px;">Your Favorite Pokemon</h1>
         <div class="listBox">
             <ul id="favoritesList">
-                <?php
-                $statement = $db->prepare("SELECT pokemon_id FROM user_favorites WHERE user_id=:userId");
-                $statement->bindValue(':userId', $_SESSION['user_id']);
-                $statement->execute();
 
-                $favoritesArray = array();
-
-                while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-
-                    array_push($favoritesArray, $row['pokemon_id']);
-                }
-
-                ?>
             </ul>
         </div>
     </div>
-    <script src="favorites.js">
+    <script>
         var favoritesArray = <?php echo json_encode($favoritesArray); ?>
         getFavoritePokemon(favoritesArray);
+
+        function getJSON(url) {
+            return fetch(url).then(function(response) {
+                if (!response.ok) {
+                    throw Error(response.statusText);
+                } else {
+                    return response.json();
+                }
+            }).catch(function(error) {
+                console.log(error);
+            })
+        }
+
+        function getFavoritePokemon(pokemonId) {
+            getJSON("https://pokeapi.co/api/v2/pokemon?limit=807&offset=0%22").then(function(data) {
+                console.log(data);
+
+                let favoriteList = document.getElementById("favoriteList");
+                console.log(data);
+                data["results"].forEach(function(value) {
+                    getJSON(value.url).then(function(pkm) {
+
+                        pokemonId.forEach(function(element) {
+
+                            if (element == pkm.id) {
+                                //console.log(value);
+                                let item = document.createElement("li");
+                                let uppercaseName = value.name.charAt(0).toUpperCase() + value.name.slice(1);
+                                let div = document.createElement("div");
+                                div.classList.add("pokemonName");
+                                div.dataset.url = value.url;
+                                div.innerHTML = `${uppercaseName} <div class="details hide ${value.name}Div">
+                <img class="pokeimg ${value.name}" src="" />
+                </div>`;
+                                let btn = document.createElement("button");
+                                btn.innerHTML = `Remove ${uppercaseName} from Favorites`;
+                                btn.dataset.pokemon = pkm.id;
+                                btn.dataset.pkmName = pkm.name;
+                                btn.dataset.url = value.url;
+                                btn.classList.add("btnRemove");
+
+                                item.appendChild(div);
+                                item.appendChild(btn);
+                                favoriteList.appendChild(item);
+
+                                div.addEventListener("click", toggleDetails);
+                                btn.addEventListener("click", removeFromFavorites);
+                                // div.querySelector(".pokeimg").addEventListener("click", showExtraDetails)
+                            }
+                        })
+                    });
+                });
+                console.log("finished JSON");
+            });
+
+        }
+
+        function removeFromFavorites(event) {
+            var pokemon_id = event.target.dataset.pokemon;
+            xhttp = new XMLHttpRequest();
+
+            var query = ('pokemonId=' + pokemon_id);
+            xhttp.open('POST', 'removeFavorite.php', true);
+            xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            xhttp.send(query);
+        }
     </script>
 
 </html>
